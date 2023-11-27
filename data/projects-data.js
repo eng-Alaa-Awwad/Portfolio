1676a63d861351f8f8b6b8e8ddc8c53c4e14d29a
eng-Alaa-Awwad <se.alaaawwad@gmail.com>
2023-11-27 11:07:11 +0300

    add quiz-v1 project

--- data/projects-data.js
+++ data/projects-data.js
@@ -5,6 +5,12 @@ const projects = [
     previewLink: "https://eng-alaa-awwad.github.io/Pig-game/",
     image: "Pig-game.png",
   },
+  {
+    name: "quiz-template-v1",
+    githubLink: "https://github.com/eng-Alaa-Awwad/quiz-template-v1",
+    previewLink: "https://eng-alaa-awwad.github.io/quiz-template-v1/",
+    image: "quiz-template-v1",
+  },
 ];
 
 export default projects;
